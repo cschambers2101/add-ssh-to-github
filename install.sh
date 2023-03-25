3a901e47c5ca9d5f7ce170cf6acbd64147fb4e34
Craig Chambers <craigchambers2101@gmail.com>
2023-03-25 09:23:23 +0000

    changed to ed25519

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ read -p "What is your email address? " emailaddress
 echo 'You entered: '${emailaddress}
 echo 'If this is not correct, press [ctrl] + [c] now to exit script'
 echo
-ssh-keygen -t rsa -b 4096 -C "$emailaddress"
+ssh-keygen -t ed25519 -C "$emailaddress"
+# ssh-keygen -t rsa -b 4096 -C "$emailaddress"
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_ed25519
+# ssh-add ~/.ssh/id_rsa
 sudo apt-get install xclip
-xclip -sel clip < ~/.ssh/id_rsa.pub
+xclip -sel clip < ~/.ssh/id_ed25519.pub
 echo
 echo
 echo 'You must now paste the key generated into your Github account. Go to github.com, then click on your icon in the top right og any page and then settings. Go to 'SSH and GPG keys', click new SSH Key, add a name for the key (machine name) and then hit [ctrl] + [v] to paste the key into the box and press 'Add SSH Key' button'
